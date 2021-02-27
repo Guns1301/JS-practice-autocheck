e8f0818aa02b382f5dfe2b29d7e4b0db675aacae
Guns1301 <67137938+Guns1301@users.noreply.github.com>
2021-02-27 22:27:12 +0200

    ...object

--- module3/autocheck-3m.js
+++ module3/autocheck-3m.js
@@ -323,7 +323,7 @@ function getAllPropValues(propName) {
 
 const products = [
   { name: 'Радар', price: 1300, quantity: 4 },
-  { n/ame: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
   { name: 'Дроид', price: 400, quantity: 7 },
   { name: 'Захват', price: 1200, quantity: 9 },
 ];
@@ -341,3 +341,156 @@ if (product.name === productName) {
 };
 
 // 21/41
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+//
+const { yesterday, today, tomorrow } = highTemperatures;
+
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// 22/41
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+
+const {
+  yesterday,
+  today,
+  tomorrow,
+  icon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg' } = highTemperatures
+
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+
+// 23/41
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+
+const {
+  yesterday: highYesterday,
+  today: highToday,
+  tomorrow: highTomorrow,
+  icon : highIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg' } = highTemperatures;
+
+const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+// 24/41
+
+const colors = [
+  { hex: '#f44336', rgb: '244,67,54' },
+  { hex: '#2196f3', rgb: '33,150,243' },
+  { hex: '#4caf50', rgb: '76,175,80' },
+  { hex: '#ffeb3b', rgb: '255,235,59' },
+];
+
+const hexColors = [];
+const rgbColors = [];
+// Для того, чтобы сократить количество повторений, можно деструктуризировать свойства объекта в локальные переменные в теле цикла.
+// Если в объекте немного свойств, деструктуризацию можно выполнить прямо в месте объявления переменной color 
+
+for (const { hex, rgb } of colors) {
+  hexColors.push(hex);
+  rgbColors.push(rgb);
+  
+  }
+
+//  25/41
+
+const forecast = {
+  today: {
+    low: 28,
+    high: 32,
+    icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+  },
+  tomorrow: {
+    low: 27,
+    high: 31,
+  },
+};
+// глубокая деструктуризация объекта
+const { 
+  // присвоение локальной перенной другое имя low: lowToday
+today: { low: lowToday, high : highToday , icon: todayIcon = 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg'},
+  // присвоение значения несуществующей переменной - icon: todayIcon =
+tomorrow: {low: lowTomorrow,high:  highTomorrow, icon: tomorrowIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg'}
+
+} = forecast
+
+// 26/41
+
+// Если функция принимает более двух-трёх аргументов, очень просто запутаться в какой последовательности что передавать. В результате получается очень неочевидный код в месте её вызова.
+function calculateMeanTemperature(forecast) {
+
+// Паттерн «Объект настроек» помогает решить эту проблему, заменяя набор параметров всего одним - объектом с именованными свойствами.
+  const { 
+    today: { low: todayLow, high : todayHigh },
+    tomorrow: {low: tomorrowLow,high: tomorrowHigh,}
+     
+  } = forecast 
+ 
+  return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+}
+
+// 27/41
+// Синтаксис ... (spread) позволяет распылить коллекцию элементов (массив, строку или объект) в место, где ожидается набор отдельных значений. Конечно есть некоторые ограничения, например нельзя распылить массив в объект и наоборот.
+
+// Можно привести аналогию с ящиком яблок. Поставив ящик на пол не вынимая из него яблоки, получим аналог массива значений. Если высыпать яблоки из ящика на пол, произойдёт распыление - набор отдельных значений.
+
+// Отличие всего одно - в JavaScript распыление не изменяет оригинальную коллекцию, то есть делается копия каждого элемента. После распыления останется и ящик полный яблок, и копия каждого яблока на полу.
+
+// Например, метод Math.max(аргументы) ищет и возвращает самый большой из аргументов (чисел), то есть ожидает не массив значений, а произвольное количество аргументов.
+const scores = [89, 64, 42, 17, 93, 51, 26];
+
+const bestScore = Math.max(...scores);
+const worstScore = Math.min(...scores);
+
+
+// 28/41
+const firstGroupScores = [64, 42, 93];
+const secondGroupScores = [89, 14, 51, 26];
+const thirdGroupScores = [29, 47, 18, 97, 81];
+// Операция spread позволяет создать копию массива или «склеить» произвольное количество массивов в один новый. Раньше для этого использовали методы slice() и concat(), но операция распыления позволяет сделать тоже самое в более краткой форме.
+const allScores = [...firstGroupScores ,...secondGroupScores,...thirdGroupScores];
+const bestScore = Math.max(...allScores);
+const worstScore = Math.min(...allScores);
+
+
+// 29/41
+// Операция spread позволяет распылить свойства произвольного количества объектов в один новый
+const defaultSettings = {
+  theme: 'light',
+  public: true,
+  withPassword: false,
+  minNumberOfQuestions: 10,
+  timePerQuestion: 60,
+};
+// Порядок распыления имеет значение. Имена свойств объекта уникальные, поэтому свойства распыляемого объекта могут перезаписать значение уже существующего свойства, если их имена совпадают
+const overrideSettings = {
+  public: false,
+  withPassword: true,
+  timePerQuestion: 30,
+};
+
+const finalSettings = { ...defaultSettings, ...overrideSettings };
+
+// 30/41
+function makeTask(data) {
+  const completed = false;
+  const category = 'Общее';
+  const priority = 'Обычный';
+    
+  return  {
+  completed, category, priority, ...data
+};
+
--- module3/practiceJs3.js
+++ module3/practiceJs3.js
@@ -9,3 +9,73 @@ const lastElement = fruits[fruits.length -1]
 console.log(fruits);
 console.log(lastElement);
 console.log(lastElementIndex);
+
+
+
+// распыление массивов
+const lastWeekTemps = [1, 2, 3];
+const currentWeekTemps = [4,5,6];
+const nextWeekTemps = [7, 8, 9];
+// spread 
+const allTemps = [...lastWeekTemps, ...currentWeekTemps, ...nextWeekTemps];
+console.log(allTemps);
+// concat = тоже самое
+const allT = lastWeekTemps.concat(currentWeekTemps, nextWeekTemps);
+
+
+
+// распыление объектов
+
+const a = { x: 1, y: 2, };
+const b = { x: 0, z: 3 };
+
+// старый метод 
+// const с = Object.assign({}, a, b);
+// первым передаем тот объект куда распылить , а следующими передаем те объекты которые хотите туда распылить 
+// const с = {} ; Object.assign(c, a, b); = таже самая запись
+
+// модный способ 
+
+const с = {
+    ...a,
+    ...b,
+};
+
+
+
+// Деструктуризация 
+const playlist = {
+
+    name: 'мой плейлист',
+    rating: 5,
+    tracks: [ 'track-1', 'track-2', 'track-3'],
+    trackCount: 3,
+    
+};
+
+const { name, rating, tracks, trackCount : numberOfTracks = 0, author = 'Слэш' } = playlist;
+// если нужно присвоить значение по умолчанию то пищем значение автор = равно и значение свойства
+// что бы переименовать переменную trackCount : numberOfTracks
+// буквально в локальную переменную numberOfTracks положи значение trackCount из объекта playlist
+// если нужно задать дефолтное значение = 0 
+console.log(playlist);
+
+
+// глубокая деструктуризация 
+
+const profile = {
+
+    nameUser: 'Mango',
+    location: 'Odessa, Ukraine',
+    avatar: null,
+        stats : {
+        likes: 5,
+        followers: 12,
+    }
+};
+
+const { nameUser, location, stats: { likes,followers } } = profile
+// followers : myFollowers = 7
+console.log(profile);
+// Деструктуризация массивов 
+
